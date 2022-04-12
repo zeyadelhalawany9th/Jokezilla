6c4c199c9bf5e1348a57284462339ff7f743fd04
Zeyad El-Halawany <whatelseisthere.5@gmail.com>
2022-04-12 23:38:23 +0200

    final updates

---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int ID { get; set; }
+        public string jokeQuestion { get; set; }
+        public string jokeAnswer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+
+
+
+    }
+}
